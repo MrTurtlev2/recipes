382e1196576bdbe9504f757a72482eeb5717db08
Marek <wojnarmarek0@gmail.com>
2022-06-21 21:14:06 +0200

    basic service

---
+++ src/main/java/MrTurtle/recipes/service/RecipeController.java
@@ -0,0 +1,23 @@
+package MrTurtle.recipes.service;
+
+import MrTurtle.recipes.model.Recipe;
+import MrTurtle.recipes.repository.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/recipes")
+public class RecipeController {
+
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+    @GetMapping(path = "/all")
+    public List<Recipe> GetAllRecipes() {
+        return recipeRepository.findAll();
+    }
+}
