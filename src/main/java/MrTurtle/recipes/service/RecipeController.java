d4fdc42d0a7b0052e49793b82ce2aa84314c2aca
Marek <wojnarmarek0@gmail.com>
2022-06-21 21:19:32 +0200

    get recipe by id endpoint

--- src/main/java/MrTurtle/recipes/service/RecipeController.java
+++ src/main/java/MrTurtle/recipes/service/RecipeController.java
@@ -4,6 +4,7 @@ import MrTurtle.recipes.model.Recipe;
 import MrTurtle.recipes.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,12 @@ public class RecipeController {
     private RecipeRepository recipeRepository;
 
     @GetMapping(path = "/all")
-    public List<Recipe> GetAllRecipes() {
+    public List<Recipe> getAllRecipes() {
         return recipeRepository.findAll();
     }
+    @GetMapping(path = "/id/{id}")
+    public Recipe getRecipeById(@PathVariable("id") int id) {
+        return  recipeRepository.getReferenceById(id);
+    }
+
 }
